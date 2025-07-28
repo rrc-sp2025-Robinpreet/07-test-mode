90f49f24ba8764f19f29d26331a07c5057df8bc8
Robinpreet kaur <Rrobinpreetkaur30@rrc.ca>
2025-07-28 09:22:21 -0500

    Added documentation for Book class: issue #2

--- developer_2/developer_2.py
+++ developer_2/developer_2.py
@@ -1,5 +1,14 @@
 class Book:
 
+    """
+    Book:  Maintains the title and author attributes of a book.
+ 
+    Attributes:
+        _title (str): The title of the book.
+        _author (str): The author of the book.
+
+    """
+
     def __init__(self, title, author):
         self.__title = title
         self.__author = author
